30ee10038c240fb8dfd1d5e88f29a8a12c9b32c9
Zaid Nawaz <zaidalviza786@gmail.com>
2026-02-21 03:02:00 +0500

    Landing page fixes

--- .next/static/chunks/pages/index.js
+++ .next/static/chunks/pages/index.js
@@ -1,9 +1,9 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/[root of the server]__fe939588._.js",
+  "static/chunks/[root of the server]__1f9119bb._.js",
   "static/chunks/node_modules_next_20c65739._.js",
   "static/chunks/node_modules_react-dom_82bb97c6._.js",
   "static/chunks/node_modules_framer-motion_dist_es_50ee6aaf._.js",
   "static/chunks/node_modules_80ad9c5a._.js",
   "static/chunks/pages_index_5771e187._.js",
-  "static/chunks/pages_index_4c3fdea2._.js"
+  "static/chunks/pages_index_f39d0da9._.js"
 ])
